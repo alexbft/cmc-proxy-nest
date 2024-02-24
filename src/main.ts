107d17ffa7129e2ab0d20ee009b86dfc5e99d7e1
alexbft <alexbft@users.noreply.github.com>
2024-02-24 15:30:06 +0100

    using https

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
+import { readFileSync } from 'fs';
+import path from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const certDir = path.resolve(__dirname, '../cert');
+  const httpsOptions = {
+    key: readFileSync(path.resolve(certDir, 'key.pem')),
+    cert: readFileSync(path.resolve(certDir, 'cert.pem')),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   const configService = app.get(ConfigService);
   const port = configService.getOrThrow('port') as number;
   Logger.log(`Listening on port ${port}`);
